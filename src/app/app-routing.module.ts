aef21e31cee2e4cbb1ae11d6f47e2ae25f503191
Andr3sRicard0 <ricardoandrescorreo@gmail.com>
2024-08-29 22:08:46 -0500

    Terminado DashBoard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'home/dashboard', pathMatch:"full"},
   {path:'home', redirectTo:'home/dashboard', pathMatch:"full"},
   {path:'home', component:HomeComponent, children:[
-    {path:'dashboard', component:DashboardComponent}
+    {path:'dashboard', component:DashboardComponent},
+    {path:'productos', component:ProductosComponent}
   ]},
   {path:'notFound', component:NotFoundComponent},
   {path:'**', redirectTo:'notFound', pathMatch:"full"},
